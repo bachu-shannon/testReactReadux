1bcf8b3f5c02b61bde9a3caf84bb8bc010fb56e1
bachu_shannon <ba4yrik@gmail.com>
2017-09-07 23:49:56 +0300

    _REACT-REDUX_

--- src/components/Add.js
+++ src/components/Add.js
@@ -28,128 +28,130 @@ class Add extends React.Component {
 
     render() {
         return (
-            <div>
+            <row>
                 <Navigation />
-                {this.props.newUser.name} {this.props.success}
-                <Form horizontal onSubmit={this.handleSubmit.bind(this)}>
-                    <FormGroup controlId="formHorizontalEmail">
-                        <Col componentClass={ControlLabel} sm={2}>
-                            name
-                        </Col>
-                        <Col sm={4}>
-                            <FormControl
-                                type="text"
-                                placeholder="Enter name"
-                                inputRef={ref => { this.name = ref; }}
-                            />
-                        </Col>
-                    </FormGroup>
-                    <FormGroup controlId="formHorizontalPassword">
-                        <Col componentClass={ControlLabel} sm={2}>
-                            gender
-                        </Col>
-                        <Col sm={4}>
-                            <FormControl
-                                type="text"
-                                placeholder="Enter gender"
-                                inputRef={ref => { this.gender = ref; }}
-                            />
-                        </Col>
-                    </FormGroup>
-                    <FormGroup>
-                        <Col componentClass={ControlLabel} sm={2}>
-                            birthYear
-                        </Col>
-                        <Col sm={4}>
-                            <FormControl
-                                type="text"
-                                placeholder="Enter birthYear"
-                                inputRef={ref => { this.birthYear = ref; }}
-                            />
-                        </Col>
-                    </FormGroup>
-                    <FormGroup>
-                        <Col componentClass={ControlLabel} sm={2}>
-                            birthMonth
-                        </Col>
-                        <Col sm={4}>
-                            <FormControl
-                                type="text"
-                                placeholder="Enter birthMonth"
-                                inputRef={ref => { this.birthMonth = ref; }}
-                            />
-                        </Col>
-                    </FormGroup>
-                    <FormGroup>
-                        <Col componentClass={ControlLabel} sm={2}>
-                            birthDay
-                        </Col>
-                        <Col sm={4}>
-                            <FormControl
-                                type="text"
-                                placeholder="Enter birthDay"
-                                inputRef={ref => { this.birthDay = ref; }}
-                            />
-                        </Col>
-                    </FormGroup>
-                    <FormGroup>
-                        <Col componentClass={ControlLabel} sm={2}>
-                            phone
-                        </Col>
-                        <Col sm={4}>
-                            <FormControl
-                                type="text"
-                                placeholder="Enter phone"
-                                inputRef={ref => { this.phone = ref; }}
-                            />
-                        </Col>
-                    </FormGroup>
-                    <FormGroup>
-                        <Col componentClass={ControlLabel} sm={2}>
-                            email
-                        </Col>
-                        <Col sm={4}>
-                            <FormControl
-                                type="text"
-                                placeholder="Enter email"
-                                inputRef={ref => { this.email = ref; }}
-                            />
-                        </Col>
-                    </FormGroup>
-                    <FormGroup>
-                        <Col componentClass={ControlLabel} sm={2}>
-                            address
-                        </Col>
-                        <Col sm={4}>
-                            <FormControl
-                                type="text"
-                                placeholder="Enter address"
-                                inputRef={ref => { this.address = ref; }}
-                            />
-                        </Col>
-                    </FormGroup>
-                    <FormGroup>
-                        <Col componentClass={ControlLabel} sm={2}>
-                            description
-                        </Col>
-                        <Col sm={4}>
-                            <FormControl
-                                componentClass="textarea"
-                                placeholder="Enter description"
-                                inputRef={ref => { this.description = ref; }}
-                            />
-                        </Col>
-                    </FormGroup>
+                <Col sm={12}>
+                    {this.props.success}
+                    <Form horizontal onSubmit={this.handleSubmit.bind(this)}>
+                        <FormGroup controlId="formHorizontalEmail">
+                            <Col componentClass={ControlLabel} sm={1}>
+                                name
+                            </Col>
+                            <Col sm={4}>
+                                <FormControl
+                                    type="text"
+                                    placeholder="Enter name"
+                                    inputRef={ref => { this.name = ref; }}
+                                />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup controlId="formHorizontalPassword">
+                            <Col componentClass={ControlLabel} sm={1}>
+                                gender
+                            </Col>
+                            <Col sm={4}>
+                                <FormControl
+                                    type="text"
+                                    placeholder="Enter gender"
+                                    inputRef={ref => { this.gender = ref; }}
+                                />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup>
+                            <Col componentClass={ControlLabel} sm={1}>
+                                birthYear
+                            </Col>
+                            <Col sm={4}>
+                                <FormControl
+                                    type="text"
+                                    placeholder="Enter birthYear"
+                                    inputRef={ref => { this.birthYear = ref; }}
+                                />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup>
+                            <Col componentClass={ControlLabel} sm={1}>
+                                birthMonth
+                            </Col>
+                            <Col sm={4}>
+                                <FormControl
+                                    type="text"
+                                    placeholder="Enter birthMonth"
+                                    inputRef={ref => { this.birthMonth = ref; }}
+                                />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup>
+                            <Col componentClass={ControlLabel} sm={1}>
+                                birthDay
+                            </Col>
+                            <Col sm={4}>
+                                <FormControl
+                                    type="text"
+                                    placeholder="Enter birthDay"
+                                    inputRef={ref => { this.birthDay = ref; }}
+                                />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup>
+                            <Col componentClass={ControlLabel} sm={1}>
+                                phone
+                            </Col>
+                            <Col sm={4}>
+                                <FormControl
+                                    type="text"
+                                    placeholder="Enter phone"
+                                    inputRef={ref => { this.phone = ref; }}
+                                />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup>
+                            <Col componentClass={ControlLabel} sm={1}>
+                                email
+                            </Col>
+                            <Col sm={4}>
+                                <FormControl
+                                    type="text"
+                                    placeholder="Enter email"
+                                    inputRef={ref => { this.email = ref; }}
+                                />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup>
+                            <Col componentClass={ControlLabel} sm={1}>
+                                address
+                            </Col>
+                            <Col sm={4}>
+                                <FormControl
+                                    type="text"
+                                    placeholder="Enter address"
+                                    inputRef={ref => { this.address = ref; }}
+                                />
+                            </Col>
+                        </FormGroup>
+                        <FormGroup>
+                            <Col componentClass={ControlLabel} sm={1}>
+                                description
+                            </Col>
+                            <Col sm={4}>
+                                <FormControl
+                                    componentClass="textarea"
+                                    placeholder="Enter description"
+                                    inputRef={ref => { this.description = ref; }}
+                                />
+                            </Col>
+                        </FormGroup>
 
-                    <FormGroup>
-                        <Col smOffset={2} sm={4}>
-                            <Button type="submit">
-                                ADD
-                            </Button>
-                        </Col>
-                    </FormGroup>
-                </Form>
-            </div>
+                        <FormGroup>
+                            <Col smOffset={2} sm={4}>
+                                <Button type="submit">
+                                    ADD
+                                </Button>
+                            </Col>
+                        </FormGroup>
+                    </Form>
+                </Col>
+            </row>
         )
 
     }
